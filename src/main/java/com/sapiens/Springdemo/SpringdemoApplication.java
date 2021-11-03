b39c2113d82e966075fde25e1bd7d4470f4b88c9
NaveShellysapiens <nave.shelly@sapiens.com>
2021-11-03 14:29:03 +0200

    constructorBasedDI

--- src/main/java/com/sapiens/Springdemo/SpringdemoApplication.java
+++ src/main/java/com/sapiens/Springdemo/SpringdemoApplication.java
@@ -1,4 +1,5 @@
 package com.sapiens.Springdemo;
+import com.sapiens.Springdemo.di.ConstructorBasedDI;
 import com.sapiens.Springdemo.ioc.Address;
 import com.sapiens.Springdemo.ioc.Contact;
 import com.sapiens.Springdemo.ioc.Employee;
@@ -6,6 +7,7 @@ import com.sapiens.Springdemo.ioc.Official_Details;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
@@ -14,6 +16,17 @@ public class SpringdemoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringdemoApplication.class, args);
+		// Task 2.1 constructorBasedDI
+		System.out.println("Constructor Based DI");
+		AnnotationConfigApplicationContext context2 = new AnnotationConfigApplicationContext(ConstructorBasedDI.class);
+        ConstructorBasedDI.AccountServiceClient bean = context2.getBean(ConstructorBasedDI.AccountServiceClient.class);
+		bean.showAccountDetails();
+		// Task 2.2 setterBasedDI
+
+
+
+
+		// Task 1
 		XmlBeanFactory factory= new XmlBeanFactory(new ClassPathResource("Beans.xml"));
 		for(int i=1;i<5;i++)
 		{
@@ -25,10 +38,10 @@ public class SpringdemoApplication {
 			Address add = (Address) factory.getBean(Address);
 			Contact contact=(Contact) factory.getBean(cont);
 			Official_Details OFD= (Official_Details) factory.getBean(OD);
-			System.out.println("**********************"+employee+"********************************");
+			System.out.println("********************************"+employee+"********************************");
 			System.out.println("Hi My Details are: "+emp.getName()+" " + emp.getID()+ " " + emp.getAge()+" "+emp.getGender() +"\n");
 			System.out.println("My Address is: "+add.getCountry()+ " " + add.getCity()+ " " + add.getPincode()+ " "+add.getPO_BOX_NUMBER()+ " "+add.getState()+ " " +"\n");
-			System.out.println(" My Contacts details are: "+contact.getEmail_id()+ " " + contact.getEmergency_Contact_no()+ " " + contact.getOfficial_mail_id()+ " "+contact.getPrimary_Mobile_no()+ " "+contact.getSecondary_Mobile_no()+ " " +"\n");
+			System.out.println("My Contacts details are: "+contact.getEmail_id()+ " " + contact.getEmergency_Contact_no()+ " " + contact.getOfficial_mail_id()+ " "+contact.getPrimary_Mobile_no()+ " "+contact.getSecondary_Mobile_no()+ " " +"\n");
 			System.out.println("My Official_details are: "+OFD.getCurrent_company_name()+ " " + OFD.getDesignation()+ " " + OFD.getEdu_qualification()+ " "+OFD.getPercentage()+ " "+ " "+OFD.getYears_of_experience()+ " " +OFD.getCurrent_company_name()+ " " +"\n");
 		}
 	}
