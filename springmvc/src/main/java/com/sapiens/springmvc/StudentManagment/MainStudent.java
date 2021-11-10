76b88a8ecf855188cb669d7fba10bab3bd072d64
NaveShellysapiens <93645401+NaveShellysapiens@users.noreply.github.com>
2021-11-10 17:20:01 +0200

    last change

---
+++ springmvc/src/main/java/com/sapiens/springmvc/StudentManagment/MainStudent.java
@@ -0,0 +1,11 @@
+package com.sapiens.springmvc.StudentManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainStudent {
+    public static void main(String[] args) {
+        SpringApplication.run(MainStudent.class,args);
+    }
+}
