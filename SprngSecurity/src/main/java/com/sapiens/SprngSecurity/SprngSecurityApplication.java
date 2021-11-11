043754b394995ed1e6041f653ccd17f562e25b48
NaveShellysapiens <93645401+NaveShellysapiens@users.noreply.github.com>
2021-11-11 09:01:52 +0200

    fixed folders

---
+++ SprngSecurity/src/main/java/com/sapiens/SprngSecurity/SprngSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sapiens.SprngSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprngSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprngSecurityApplication.class, args);
+	}
+
+}
---
+++ SprngSecurity/src/test/java/com/sapiens/SprngSecurity/SprngSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapiens.SprngSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprngSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
